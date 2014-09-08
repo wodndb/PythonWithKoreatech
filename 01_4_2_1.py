759f6e962f303e1fcc8e8762d39e0f1fd71a7d84
wodndb <wodndb@koreatech.ac.kr>
2014-09-08 12:09:20 +0900

    01_4_2_1.py Create!

---
+++ 01_4_2_1.py
@@ -0,0 +1,9 @@
+#!/usr/local/bin/python
+# coding: utf-8
+
+import sys, os
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3, 4, 5, 6], [1, 4, 9, 16, 25, 36])
+plt.ylabel('some numbers')
+plt.show()
