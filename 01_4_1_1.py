843923288659d3e9087fb62179e38c32e2627f05
wodndb <wodndb@koreatech.ac.kr>
2014-09-08 12:08:49 +0900

    01_4_2_1.py Create!

---
+++ 01_4_1_1.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python
+# coding: utf-8
+import os, sys
+
+from turtle import *
+reset()
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
